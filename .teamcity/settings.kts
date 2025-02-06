435fd5070bd14c25626dffe2429f92a912f6f6d9
admin <admin@TeamCity>
2025-02-06 13:07:51 +0000

    TeamCity change in 'NewRoot / SubProject_1' project: runners of 'TestCommands' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -46,15 +45,6 @@ object SubProject1_TestCommands : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            pomLocation = ".teamcity/pom.xml"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
     }
 })
 
