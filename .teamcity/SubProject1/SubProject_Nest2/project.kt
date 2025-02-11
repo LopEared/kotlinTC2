c5a791d240c1b4f7f974a3c0104797f745c3fa1e
LopEared <mikuser@DESKTOP-MIKE>
2025-02-11 13:54:05 +0300

    test_23

--- .teamcity/SubProject1/SubProject_Nest2/project.kt
+++ .teamcity/SubProject1/SubProject_Nest2/project.kt
@@ -2,8 +2,8 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object SubProject_Nest2 : Project({
-    name = "SubProject_Nest2"
+    name = "SubProject_Nest2_Project"
     description = "Test Dependcies from subproject"
 
-    buildType(SubProject_Nest2)
+    buildType(SubProject_Nest2_buildConfig)
 })
