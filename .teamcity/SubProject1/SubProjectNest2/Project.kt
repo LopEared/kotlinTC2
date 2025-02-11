0bb9a21619403028d68279207ff5681b84b760d5
LopEared <mikuser@DESKTOP-MIKE>
2025-02-11 16:31:21 +0300

    test_23

--- .teamcity/SubProject1/SubProjectNest2/Project.kt
+++ .teamcity/SubProject1/SubProjectNest2/Project.kt
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.Project
+import SubProject1.SubProjectNest2.buildTypes.TestDependcyFromSubProject
 
 object SubProjectNest2 : Project({
     id("SubProject1_SubProjectNest2")
