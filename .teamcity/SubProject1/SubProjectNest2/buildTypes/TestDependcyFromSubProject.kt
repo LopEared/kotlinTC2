c9a65b0f79742d42a8f56792a50f78d698be0093
LopEared <mikuser@DESKTOP-MIKE>
2025-02-11 16:41:29 +0300

    test_23

--- .teamcity/SubProject1/SubProjectNest2/buildTypes/TestDependcyFromSubProject.kt
+++ .teamcity/SubProject1/SubProjectNest2/buildTypes/TestDependcyFromSubProject.kt
@@ -1,7 +1,7 @@
-// import jetbrains.buildServer.configs.kotlin.*
-// import jetbrains.buildServer.configs.kotlin.ui.*
-// import jetbrains.buildServer.configs.kotlin.buildSteps.script
-// import jetbrains.buildServer.configs.kotlin.BuildTypeSettings.Type.REGULAR
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.ui.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.BuildTypeSettings.Type.REGULAR
 
 // object SubProject_Nest2_buildConfig : BuildType({
 //     name = "SubProject_Nest2_buildConfig"
@@ -24,15 +24,40 @@
 //     }
 // })
 
-// private fun BuildSteps.SubDependcyFunction() {
-//     script {
-//         name = "SubDependcyFunction_NAME"
-//         id = "SubDependcyFunction_ID"
-//         workingDir = "/"
-//         scriptContent = """
-//                 #!/bin/bash
-//                 echo "##teamcity[message text='<<< TEST WARNING MESSAGE!!! >>>' status='WARNING']"
-//                 echo "Notes From SubProject Dependcy Function: " >  %build.number%_%teamcity.build.branch%_subproj_dependcy.txt
-//                 """.trimIndent()
-//     }
-// }
+
+
+object SubProject_Nest2_buildConfig : BuildType({
+    id("SubProject1_SubProjectNest3_SubProjectNest3BuildConfig")
+    name = "SubProjectNest3_BuildConfig"
+    description = "BuildConfig_SubProjectNest3"
+
+    type = REGULAR
+    enablePersonalBuilds = false
+    maxRunningBuilds = 1
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        SubDependcyFunction()
+    }
+
+    requirements {
+        equals("teamcity.agent.name", "ip_172.17.0.1")
+    }
+})
+
+
+private fun BuildSteps.SubDependcyFunction() {
+    script {
+        name = "SubDependcyFunction_NAME"
+        id = "SubDependcyFunction_ID"
+        workingDir = "/"
+        scriptContent = """
+                #!/bin/bash
+                echo "##teamcity[message text='<<< TEST WARNING MESSAGE!!! >>>' status='WARNING']"
+                echo "Notes From SubProject Dependcy Function: " >  %build.number%_%teamcity.build.branch%_subproj_dependcy.txt
+                """.trimIndent()
+    }
+}
