2305c944754cb6ef01248af1d647545b0f2b0b9a
LopEared <mikuser@DESKTOP-MIKE>
2025-02-12 17:28:20 +0300

    test_28

--- .teamcity/SubProject1/buildTypes/TestCommands.kt
+++ .teamcity/SubProject1/buildTypes/TestCommands.kt
@@ -64,7 +64,7 @@ object SubProject1_TestCommands : BuildType({
     steps {
         checkPassword()
         confirmVcsBranch()
-        if (%TestCheckBox% == true){
+        if (%TestCheckBox%){
             createFile()
         }
 
