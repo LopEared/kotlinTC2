f65a4eb6586da8a56639ba6f6321f877cfe8e939
LopEared <mikuser@DESKTOP-MIKE>
2025-02-14 14:15:18 +0300

    test_30

--- .teamcity/SubProject1/buildTypes/maintainFunc.kt
+++ .teamcity/SubProject1/buildTypes/maintainFunc.kt
@@ -10,7 +10,7 @@ fun BuildSteps.PlugScreenUP() {
             #!/bin/bash
             if [ -f /maintenance-stage.html2 ]; then
                 echo "TURN ON PLUG SCREEN"
-                sudo bash -c "cd / && mv maintenance-stage.html2 maintenance-stage.html || exit 1"
+                bash -c "cd / && mv maintenance-stage.html2 maintenance-stage.html || exit 1"
             else
                 echo "##teamcity[message text='PAGE FILE NOT FOUND!!! SCREEN PLUG STATE DO NOT CHANGED!!!' status='WARNING']"
             fi
@@ -31,7 +31,7 @@ fun BuildSteps.PlugScreenDOWN() {
             #!/bin/bash
             if [ -f /maintenance-stage.html ]; then
                 echo "TURN OFF PLUG SCREEN"
-                sudo bash -c "cd / && mv maintenance-stage.html maintenance-stage.html2 || exit 1"
+                bash -c "cd / && mv maintenance-stage.html maintenance-stage.html2 || exit 1"
             else
                 echo "##teamcity[message text='PAGE FILE NOT FOUND!!! SCREEN PLUG STATE DO NOT CHANGED!!!' status='WARNING']"
             fi
