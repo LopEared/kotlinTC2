91896ead6412d2e9c5c1587e7f8f24bd2477bdda
conmmik <conmmik@partner.waters.com>
2025-03-07 11:12:11 +0300

    test_71

--- .teamcity/SubProject1/buildTypes/TestCommands.kt
+++ .teamcity/SubProject1/buildTypes/TestCommands.kt
@@ -16,22 +16,19 @@ object SubProject1_TestCommands : BuildType({
     enablePersonalBuilds = false
     maxRunningBuilds = 1
 
+    // val envFile = "TEST NOTE!"
+    // text("confirmVcsBranchName", "", label = "Confirm branch name:", display = ParameterDisplay.PROMPT, allowEmpty = false)
+    // param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "", label = "Confirm branch name:", display = ParameterDisplay.PROMPT, allowEmpty = false )
+    // param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "TEST_IS_MY" )
+
     params {
         password("deploy_pass", "credentialsJSON:3d93ad41-aacd-4523-8208-9f4cb2ca9531", display = ParameterDisplay.HIDDEN, readOnly = true)
         password("password", "", label = "Password", description = "Input password to start build", display = ParameterDisplay.PROMPT)
-        
-        param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "%teamcity.build.branch%")
-        
         text("confirmVcsBranchName", "", label = "Confirm branch name:", display = ParameterDisplay.PROMPT, allowEmpty = false)
-
         checkbox("TestCheckBox", "true", label = "Screen plug during process", description = "Will put up a screen plug before deployment and removed it after deployment.", display = ParameterDisplay.PROMPT, checked = "true", unchecked = "false")
         checkbox("pgsqlMakeBackup", "true", label = "Backup Postgres DB", description = "Make Backup for Postgres DB:consult and appoinment.", display = ParameterDisplay.PROMPT, checked = "true", unchecked = "false")
-        // val envFile = "TEST NOTE!"
+        param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "%teamcity.build.branch%")
         param("env.envFile", getTextFromFile("SubProject1/recources/testFile.txt"))
-    
-        text("confirmVcsBranchName", "", label = "Confirm branch name:", display = ParameterDisplay.PROMPT, allowEmpty = false)
-        // param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "", label = "Confirm branch name:", display = ParameterDisplay.PROMPT, allowEmpty = false )
-        // param("reverse.dep.${DependencyConfig_Test.id}.confirmVcsBranchName", "TEST_IS_MY" )
     }
 
     vcs {
